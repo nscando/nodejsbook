4711f5b109ad3a2999c00c141e03ce64a3e486f0
nscando <nicolas.scandizzo@gmail.com>
2020-12-03 09:09:19 -0300

    Initial commit - Proyect config

---
+++ api/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const config = require('../config.js');
+const user = require('./components/user/network');
+
+const app = express();
+
+
+//ROUTES
+
+app.use('/api/user', user)
+
+app.listen(config.api.port, () => {
+     console.log(`Listening on port http://localhost:${config.api.port}`);
+});
