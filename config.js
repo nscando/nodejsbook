fe1a82f13f77d92242d591bad35514240d13938d
nscando <nicolas.scandizzo@gmail.com>
2020-12-06 09:59:20 -0300

    DB config- MYSQL

--- config.js
+++ config.js
@@ -4,5 +4,12 @@ module.exports = {
      },
      jwt: {
           secret: process.env.JWT_SECRET || 'notesecret'
+     },
+     mysql: {
+          host: process.env.MYSQL_HOST || 'remotemysql.com',
+          user: process.env.MYSQL_USER || 'hutFuvm9OX',
+          password: process.env.MYSQL_PASS || 'vH8ucLLB4B',
+          database: process.env.MYSQL_DB || 'hutFuvm9OX',
+
      }
 };
