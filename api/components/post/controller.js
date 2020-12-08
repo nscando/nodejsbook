7b9fee32ccca09d2d809981349c642fa040400ce
nscando <nicolas.scandizzo@gmail.com>
2020-12-08 12:25:47 -0300

    POSTS

---
+++ api/components/post/controller.js
@@ -0,0 +1,17 @@
+const auth = require('../../../auth');
+const TABLA = 'post';
+
+module.exports = function (injectedStore) {
+  let store = injectedStore;
+  if (!store) {
+    store = require('../../../store/dummy');
+  }
+
+  function list() {
+    return store.list(TABLA)
+  }
+
+  return {
+    list
+  }
+};
--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ const swaggerUi = require('swagger-ui-express')
 const config = require('../config.js');
 const user = require('./components/user/network');
 const auth = require('./components/auth/network');
+const post = require('./components/post/network');
 const errors = require('../network/errors');
 
 const app = express();
@@ -18,7 +19,8 @@ const swaggerDoc = require('./swagger.json');
 
 //ROUTER
 app.use('/api/user', user);
-app.use('/api/auth', auth)
+app.use('/api/auth', auth);
+app.use('/api/post', post);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.use(errors);
